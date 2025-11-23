4521bab2de1999e191c1159c9c07444d1a21ee6c
yunchan <yunchan771@naver.com>
2025-11-23 19:27:16 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int c1,c2,c3,age;
+
+    scanf("%d\n",&c3);
+    scanf("%d",&c2);
+    
+    age=c2-c3;
+    c1=age+c2;
+
+    printf("%d",c1);
+
+    return 0;
+    
+}
