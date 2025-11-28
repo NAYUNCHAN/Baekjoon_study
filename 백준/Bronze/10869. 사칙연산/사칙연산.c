a74d0464711326f20b7820ed58cf7ae88666e26d
yunchan <yunchan771@naver.com>
2025-11-28 22:47:11 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    int x=0,y=0;
+
+    scanf("%d %d",&x,&y);
+
+    printf("%d\n",x+y);
+    printf("%d\n",x-y);
+    printf("%d\n",x*y);
+    printf("%d\n",x/y);
+    printf("%d\n",x%y);
+    return 0;
+}
