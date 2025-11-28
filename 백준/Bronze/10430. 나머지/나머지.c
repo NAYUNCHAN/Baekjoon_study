bfbd993f687020527685964d970062f11cd27339
yunchan <yunchan771@naver.com>
2025-11-28 22:57:02 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+    int a=0,b=0,c=0;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    printf("%d\n%d\n%d\n%d",(a+b)%c,((a%c)+(b%c))%c,(a*b)%c,((a%c)*(b%c))%c);
+    
+    return 0;
+}
