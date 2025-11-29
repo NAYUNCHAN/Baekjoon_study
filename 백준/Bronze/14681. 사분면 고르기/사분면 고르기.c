f67642ea71242e096732bab376abb82138dacb84
yunchan <yunchan771@naver.com>
2025-11-29 11:57:14 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int x=0,y=0;
+
+    scanf("%d\n%d",&x,&y);
+
+    if(x==0 || y==0){
+        return 0;
+    }
+
+    if(x>0 && y>0){
+        printf("1");
+    }
+    else if(x>0 && y<0){
+        printf("4");
+    }
+    else if(x<0 && y>0){
+        printf("2");
+    }
+    else if(x<0 && y<0){
+        printf("3");
+    }
+
+    return 0;
+}
