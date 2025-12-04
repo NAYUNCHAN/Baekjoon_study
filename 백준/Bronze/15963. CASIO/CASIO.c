b839cca44b591ec8058519799c8533e7abe462f9
yunchan <yunchan771@naver.com>
2025-12-04 18:55:06 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n=0,m=0;
+
+    scanf("%d %d",&n,&m);
+
+    if(n==m)
+        printf("1");
+    else
+        printf("0");
+
+    return 0;
+        
+}
